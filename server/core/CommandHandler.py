0a83549c3298eeeb5d1ca441d1d04fb10038cbc8
Nicolò Rinaldi <LunarStone292@proton.me>
2025-11-19 11:27:48 +0100

    New command in CommandHandler : /mkusr

--- server/core/CommandHandler.py
+++ server/core/CommandHandler.py
@@ -5,13 +5,14 @@ class CommandHandler(object):
     def __init__(self, dbHandler: DBHandler, connHandler: ConnHandler) -> None:
         self.commands = {
             "help": (0, self.helpCommand),
-            "user_id":  (1, self.user_id),       # print user id : /user_id <username>
-            "ban":      (1, self.banUser),       # Ban a user : /ban <usr_id>
-            "unban":    (1, self.unbanUser),     # Unban a user : /unban <usr_id>
-            "isAdmin":  (1, self.isAdmin),       # Check if a user is admin : /isAdmin <usr_id>
-            "isBanned": (1, self.isBanned),      # Check if a user is banned : /isBanned <usr_id>
-            "usrpw":    (2, self.changePasswd),  # Change a password for a user : /usrpw <usr_id> <new_pwd>
+            "user_id":  (1, self.user_id),       # print user id : /user_id <username : str>
+            "ban":      (1, self.banUser),       # Ban a user : /ban <usr_id : int>
+            "unban":    (1, self.unbanUser),     # Unban a user : /unban <usr_id : int>
+            "isAdmin":  (1, self.isAdmin),       # Check if a user is admin : /isAdmin <usr_id: int>
+            "isBanned": (1, self.isBanned),      # Check if a user is banned : /isBanned <usr_id : int>
+            "usrpw":    (2, self.changePasswd),  # Change a password for a user : /usrpw <usr_id : int> <new_pwd : str>
             "lsip":     (0, self.lsip),          # List all the connected ip's : /lsusr
+            "mkusr":    (4, self.makeUser),      # Create a user on the database : /mkusr <username : str> <password : str> <email : str> <isadmin : bool>
         }
         
         self._db = dbHandler
@@ -25,13 +26,14 @@ class CommandHandler(object):
         return \
 b'''
 /help           Display this message
-/user_id        print user id : /user_id <username>
-/ban            Ban a user : /ban <usr_id>
-/unban          Unban a user : /unban <usr_id>
-/isAdmin        Check if a user is admin : /isAdmin <usr_id>
-/isBanned       Check if a user is banned : /isBanned <usr_id>
-/usrpw          Change a password for a user : /usrpw <usr_id> <new_pwd>
+/user_id        print user id : /user_id <username : str>
+/ban            Ban a user : /ban <usr_id : int>
+/unban          Unban a user : /unban <usr_id : int>
+/isAdmin        Check if a user is admin : /isAdmin <usr_id : int>
+/isBanned       Check if a user is banned : /isBanned <usr_id : int>
+/usrpw          Change a password for a user : /usrpw <usr_id : int> <new_pwd : str>
 /lsip           List all the connected ip's : /lsusr
+/mkusr          Create a user on the database : /mkusr <username : str> <password : str> <email : str> <isadmin : bool>
 '''
     
 
@@ -139,3 +141,10 @@ b'''
     
     def lsip(self) -> tuple[str]:
         return self._cn.get_all_ip()
+    
+    
+    def makeUser(self, username: str, password: str, email: str | None = None, admin: bool = False) -> bool:
+        if not username or not password or (admin and not email):
+            return False
+        
+        return self._db.makeUser(username, password, email, admin, True)
